651eaaa15ac1a96da86165c0441338657c2c88a4
dlyav <github_090506@mail.ru>
2024-12-01 23:27:09 +0500

    Task2

---
+++ Tyuiu.RyabtsevNE.Sprint5.Task2.V1.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RyabtsevNE.Sprint5.Task2.V1.Lib
+{
+    public class DataService : ISprint5Task2V1
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.txt");
+            string text = "";
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 != 0) matrix[i, j] = 0;
+                    text += $"{matrix[i, j].ToString()};";
+                }
+                text = text.Substring(0, text.Length - 1) + "\n";
+            }
+
+            File.WriteAllText(path, text);
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.RyabtsevNE.Sprint5.Task2.V1/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.RyabtsevNE.Sprint5.Task2.V1.Lib;
+namespace Tyuiu.RyabtsevNE.Sprint5.Task2.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mtrx = new int[3, 3] { {6,9,4 },
+                                          {7,2,4 },
+                                          {4,8,3 }};
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int colms = mtrx.Length / rows;
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Рябцев Н. Е. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс file. Запись структурированных данных в текстовый файл      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Рябцев Никита Евгеньевич | СМАРТб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный массив. Заменить нечетные элементы массива на 0           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colms; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
