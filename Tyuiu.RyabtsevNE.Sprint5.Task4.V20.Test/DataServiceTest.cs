52aed4605998822b397536528685aceff8080fb0
dlyav <github_090506@mail.ru>
2024-12-01 23:54:36 +0500

    Task4

--- Tyuiu.RyabtsevNE.Sprint5.Task4.V20.Test/DataServiceTest.cs
+++ Tyuiu.RyabtsevNE.Sprint5.Task4.V20.Test/DataServiceTest.cs
@@ -6,7 +6,7 @@
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\DataSprint5\InPutDataFileTask4V20.txt";
+            string path = @"C:\Users\dlyav\source\repos\Tyuiu.RyabtsevNE.Sprint5\Tyuiu.RyabtsevNE.Sprint5.Task4.V20\bin\Debug\net8.0\InPutDataFileTask4V20.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExist = fileInfo.Exists;
             bool wait = true;
